67396fe94de5475e34883487f384e5cb727f15c1
Norbert Czirjak <norbert.czirjak@oeaw.ac.at>
2024-01-15 15:52:05 +0100

    homepage changes

---
+++ src/Controller/ArcheBaseController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\arche_core_gui_api\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Description of ArcheBaseController
+ *
+ * @author nczirjak
+ */
+class ArcheBaseController extends ControllerBase
+{
+    protected $config;
+    protected $repoDb;
+    protected $siteLang;
+    protected $helper;
+    protected $model;
+
+    public function __construct()
+    {
+        
+        (isset($_SESSION['language'])) ? $this->siteLang = strtolower($_SESSION['language']) : $this->siteLang = "en";
+        $this->config = \Drupal::service('extension.list.module')->getPath('arche_core_gui') . '/config/config.yaml';
+       
+        try {
+            $this->repoDb = \acdhOeaw\arche\lib\RepoDb::factory($this->config);
+        } catch (\Exception $ex) {
+            \Drupal::messenger()->addWarning($this->t('Error during the BaseController initialization!').' '.$ex->getMessage());
+            return array();
+        }
+    }
+}
--- src/Controller/MetadataController.php
+++ src/Controller/MetadataController.php
@@ -7,12 +7,92 @@ use Symfony\Component\HttpFoundation\Response;
 use EasyRdf\Graph;
 use EasyRdf\Literal;
 
-class MetadataController extends \Drupal\arche_core_gui\Controller\ArcheBaseController {
+use acdhOeaw\arche\lib\RepoDb;
+use acdhOeaw\arche\lib\SearchConfig;
+use acdhOeaw\arche\lib\SearchTerm;
 
-    private $apiHelper;
+class MetadataController extends \Drupal\arche_core_gui_api\Controller\ArcheBaseController {
     
+    private $apiHelper;
+
     public function __construct() {
         $this->apiHelper = new \Drupal\arche_core_gui_api\Helper\ApiHelper();
+    }
+
+    public function getTopCollections_(int $count, string $lang = "en"): JsonResponse {
+  
+        $repo = RepoDb::factory(\Drupal::service('extension.list.module')->getPath('arche_core_gui') . '/config/config-gui.yaml');
+       
+$schema = $repo->getSchema();
+$scfg = new \acdhOeaw\arche\lib\SearchConfig();
+$scfg->orderBy = ['^' . $schema->modificationDate];
+$scfg->limit = 10;
+//<-- adjust according to what you need to display
+//    see https://acdh-oeaw.github.io/arche-docs/aux/metadata_api_for_programmers.html
+$scfg->metadataMode = 'resource';
+$scfg->resourceProperties = [
+    $schema->title,
+    $schema->modificationDate,
+];
+$scfg->relativesProperties = [];
+//-->
+
+// the fastest way but with more postprocessing required you get a PDO statement with each row being a metadata triple
+// and you need to map it to resource objects, see code samples e.g. in https://redmine.acdh.oeaw.ac.at/issues/21475
+$pdoStmt = $repo->getPdoStatementBySearchTerms([new \acdhOeaw\arche\lib\SearchTerm(\zozlak\RdfConstants::RDF_TYPE, $schema->classes->topCollection)], $scfg);
+// or
+// here you get a collection of RepoResourceDb objects and from each you can get its EasyRdf metadata object and read
+// data from it
+//$resources = $repo->getResourcesBySearchTerms([new \acdhOeaw\arche\lib\SearchTerm(\zozlak\RdfConstants::RDF_TYPE, $schema->classes->topCollection)], $scfg);
+
+
+while ($triple = $pdoStmt->fetchObject()) {
+         
+        $id = (string)$triple->id;
+        $property = $triple->property;
+        
+        echo "<pre>";
+        var_dump($triple);
+        echo "</pre>";
+
+     
+        if($property === "search://match") {
+            echo $id;
+            echo "<br>";
+        }
+        
+        $context = $id === $resId ? $contextResource : $contextRelatives;
+        $shortProperty = $context[$triple->property] ?? false;
+        $resources[$id] ??= (object) ['id' => $id];
+        $tid = null;
+        if ($triple->type === 'REL') {
+            $tid = $triple->value;
+            $resources[$tid] ??= (object) ['id' => $tid];
+        }
+
+        if ($triple->type !== 'REL') {
+            if ($shortProperty) {
+                // ordinary property existing in the context
+                $resources[$id]->{$shortProperty}[] = \acdhOeaw\arche\lib\TripleValue::fromDbRow($triple);
+            } elseif ($id === $resId) {
+                // expert property - out of context but belongs to the main resource 
+                $resource->expert[$property][] = \acdhOeaw\arche\lib\TripleValue::fromDbRow($triple);
+            }
+        } elseif ($shortProperty) {
+            $resources[$id]->{$shortProperty}[$tid] = $resources[$tid];
+        } elseif ($id === $resId) {
+            $resource->expert[$property][$tid] = $resources[$tid];
+        }
+}
+echo "<pre>";
+var_dump($resources);
+echo "</pre>";
+
+die();
+
+die();
+        
+       
     }
 
     /**
@@ -61,18 +141,17 @@ class MetadataController extends \Drupal\arche_core_gui\Controller\ArcheBaseCont
         $i = 0;
         foreach ($php as $k => $v) {
             if ($v['http://www.w3.org/1999/02/22-rdf-syntax-ns#type'][0]['value'] == 'https://vocabs.acdh.oeaw.ac.at/schema#TopCollection') {
-                if($i === $count) {
+                if ($i === $count) {
                     break;
                 }
                 $id = str_replace('https://arche-dev.acdh-dev.oeaw.ac.at/api/', '', $k);
                 $results[$id]['title'] = $this->apiHelper->getLangValue($v['https://vocabs.acdh.oeaw.ac.at/schema#hasTitle'], $lang);
                 $results[$id]['description'] = $this->apiHelper->getLangValue($v['https://vocabs.acdh.oeaw.ac.at/schema#hasDescription'], $lang);
                 $results[$id]['acdhid'] = $this->apiHelper->getAcdhIdValue($v['https://vocabs.acdh.oeaw.ac.at/schema#hasIdentifier']);
-                
+
                 $i++;
             }
         }
         return new JsonResponse($results);
     }
-
 }
