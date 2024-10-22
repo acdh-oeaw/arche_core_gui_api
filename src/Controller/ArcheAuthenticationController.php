fa1559fbb0a3b4428081d71b153a46556cf274cf
Norbert Czirjak <norbert.czirjak@oeaw.ac.at>
2024-10-22 15:14:50 +0200

    login changes, dissemiantion view changes, smartsearch titleimage changes

--- src/Controller/ArcheAuthenticationController.php
+++ src/Controller/ArcheAuthenticationController.php
@@ -28,28 +28,54 @@ class ArcheAuthenticationController extends \Drupal\arche_core_gui\Controller\Ar
     public function checkUserPermission(string $resId, string $aclRead = "") {
         $result = [];
         $this->actualPage = $this->repoDb->getBaseUrl() . $resId;
-        $username = "";
-        
+
+        $result = $this->checkHttpLogin($aclRead);
+
+        if (count($result) === 0) {
+            $result = $this->checkShibbolethLogin($aclRead);
+        }
+
+        if (count($result) === 0) {
+            //the user is not logged in
+            $result['access'] = "login";
+        }
+
+        $response = new Response();
+        $response->setContent(json_encode((array) $result, \JSON_PARTIAL_OUTPUT_ON_ERROR, 1024));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+
+    /**
+     * Check if the user logged in with httpd
+     * @param string $aclRead
+     * @return array
+     */
+    private function checkHttpLogin(string $aclRead): array {
         //check if the user is logged in
+        $result = [];
         if (!empty($_COOKIE[$this->config->authLoginCookie])) {
+
+            $username = "";
             $loginCookie = explode(',', $_COOKIE[$this->config->authLoginCookie]);
-           
             $username = $loginCookie[0];
             unset($loginCookie[0]);
             $this->roles = $loginCookie;
             //we passed acl rights
             $result['username'] = $username;
-            $result['roles'] = implode(', ',$this->roles);
-            if(!empty($aclRead)) {
+            $result['roles'] = implode(', ', $this->roles);
+            $result['loginMethod'] = 'httpd';
+            if (!empty($aclRead)) {
                 $aclRead = explode(',', $aclRead);
                 //the user has the right aclread group
-                if(array_intersect($aclRead, $this->roles)) {
+                if (array_intersect($aclRead, $this->roles)) {
                     $result['username'] = $username;
                     $result['access'] = "authorized";
-                } elseif(in_array('admin', $this->roles)) {
+                } elseif (in_array('admin', $this->roles)) {
                     $result['username'] = $username;
                     $result['access'] = "authorized";
-                }else {
+                } else {
                     //the user doesnt have the right group to access the resource
                     $result['access'] = "not authorized";
                 }
@@ -57,15 +83,39 @@ class ArcheAuthenticationController extends \Drupal\arche_core_gui\Controller\Ar
                 //the user doesnt have the right group to access the resource
                 $result['access'] = "not authorized";
             }
-        } else {
-            //the user is not logged in
-            $result['access'] = "login";
         }
-        
-        $response = new Response();
-        $response->setContent(json_encode((array) $result, \JSON_PARTIAL_OUTPUT_ON_ERROR, 1024));
-        $response->headers->set('Content-Type', 'application/json');
+        return $result;
+    }
 
-        return $response;
+    /**
+     * Check if the user logged in with Shibboleth
+     * @param string $aclRead
+     * @return array
+     */
+    private function checkShibbolethLogin(string $aclRead): array {
+        $result = [];
+        if (!empty($_SERVER[$this->config->authSsoHeader])) {
+            $result['username'] = $_SERVER[$this->config->authSsoHeader];
+            $this->roles[] = 'academic';
+            $this->roles[] = 'public';
+            $result['roles'] = 'academic, public';
+            $result['loginMethod'] = 'shibboleth';
+            if (!empty($aclRead)) {
+                $aclRead = explode(',', $aclRead);
+                //the user has the right aclread group
+                if (array_intersect($aclRead, $this->roles)) {
+                    $result['access'] = "authorized";
+                } elseif (in_array('admin', $this->roles)) {
+                    $result['access'] = "authorized";
+                } else {
+                    //the user doesnt have the right group to access the resource
+                    $result['access'] = "not authorized";
+                }
+            } else {
+                //the user doesnt have the right group to access the resource
+                $result['access'] = "not authorized";
+            }
+        }
+        return $result;
     }
 }
