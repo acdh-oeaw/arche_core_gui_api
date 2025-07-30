f09c43b84f98cb7895d3be4a09e65b7ef23adb45
Norbert Czirjak <norbert.czirjak@oeaw.ac.at>
2025-07-30 12:17:52 +0200

    accepted formats translation added

--- src/Helper/AcceptedFormatsHelper.php
+++ src/Helper/AcceptedFormatsHelper.php
@@ -2,12 +2,16 @@
 
 namespace Drupal\arche_core_gui_api\Helper;
 
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
 /**
  * Description of AcceptedFormatsHelper Class
  *
  * @author nczirjak
  */
 class AcceptedFormatsHelper extends \Drupal\arche_core_gui_api\Helper\ArcheCoreHelper {
+
+    use StringTranslationTrait;
 
     private $data;
     private $lang = "en";
@@ -29,9 +33,9 @@ class AcceptedFormatsHelper extends \Drupal\arche_core_gui_api\Helper\ArcheCoreH
         $html = "<table class='table table-hover'>";
         $html .= "<thead >";
         $html .= '<tr>';
-        $html .= '<th class="text-center"><b>'.$this->t("EXTENSION").'</b></th>';
-        $html .= '<th class="text-center"><b>'.$this->t("FORMAT NAME & VERSION").'</b></th>';
-        $html .= '<th class="text-center"><b>'.$this->t("PREFERENCE").'</b></th>';
+        $html .= '<th class="text-center"><b>' . $this->t("EXTENSION") . '</b></th>';
+        $html .= '<th class="text-center"><b>' . $this->t("FORMAT NAME & VERSION") . '</b></th>';
+        $html .= '<th class="text-center"><b>' . $this->t("PREFERENCE") . '</b></th>';
         $html .= "</thead >";
         $html .= '</tr>';
 
